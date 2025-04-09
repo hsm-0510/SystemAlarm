c71cf15397ad26c9f64f071e78b0f2751aed3d6f
hasemhasan <sm.hasemhasan@gmail.com>
2025-04-09 11:55:51 +0500

    Changed from list to dictionary structure

--- SystemAlarm/SystemAlarm.py
+++ SystemAlarm/SystemAlarm.py
@@ -41,17 +41,17 @@ def main():
     try:
         EA_array = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
         EE_array = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        EA_array_named = ['RAM Corrput', 'Flash Error', 'RAM Bad', 'ROM Bad',
-                          'Passcode Reset', 'System Program Error', 'Watchdog', 'Finish Backup Bad',
-                          'User Alarm 3', 'User Alarm 2', 'User Alarm 1', 'Power Fail Alarm',
-                          'Ticket Alarm', 'Communications', 'User Alarm 5', 'User Alarm 4',
-                          'Pulse Security', 'Add Clean Line', 'Overrun Alarm', 'Zero Flow Alarm',
-                          'Density Trans', 'Temp Probe', 'Back Pressure', 'Valve Fault',
-                          'High Density', 'High Temp', 'High Flow', 'Pressure Trans',
-                          'Low Density', 'Low Temp', 'Low Flow', 'High Pressure',
-                          'Mass Meter Tube', 'Mass Meter Overdrive', 'Mass Meter Comm Fail', 'Low Pressure',
-                          'Report Full Storage', 'Promass Alarm', 'Shared Printer', 'PTB Printer Failure',
-                          'Not Used', 'DV: Divert Timeout Alarm', 'SW: BS&W Probe Alarm', 'NP:Network Printer Alarm']
+        EA_dict = {'RAM Corrput':0, 'Flash Error':0, 'RAM Bad':0, 'ROM Bad':0,
+                          'Passcode Reset':0, 'System Program Error':0, 'Watchdog':0, 'Finish Backup Bad':0,
+                          'User Alarm 3':0, 'User Alarm 2':0, 'User Alarm 1':0, 'Power Fail Alarm':0,
+                          'Ticket Alarm':0, 'Communications':0, 'User Alarm 5':0, 'User Alarm 4':0,
+                          'Pulse Security':0, 'Add Clean Line':0, 'Overrun Alarm':0, 'Zero Flow Alarm':0,
+                          'Density Trans':0, 'Temp Probe':0, 'Back Pressure':0, 'Valve Fault':0,
+                          'High Density':0, 'High Temp':0, 'High Flow':0, 'Pressure Trans':0,
+                          'Low Density':0, 'Low Temp':0, 'Low Flow':0, 'High Pressure':0,
+                          'Mass Meter Tube':0, 'Mass Meter Overdrive':0, 'Mass Meter Comm Fail':0, 'Low Pressure':0,
+                          'Report Full Storage':0, 'Promass Alarm':0, 'Shared Printer':0, 'PTB Printer Failure':0,
+                          'Not Used':0, 'Divert Timeout Alarm':0, 'BS&W Probe Alarm':0, 'Network Printer Alarm':0}
         EE_array_named = ['Program Mode', 'Released', 'Flowing', 'Authorized',
                           'Transaction in Progress', 'Transaction Done', 'Batch Done', 'Keypad Data Pending',
                           'Printing in Progress', 'Permissive Delay', 'New Card Data Available', 'Alarm',
